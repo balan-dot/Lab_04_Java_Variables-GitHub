e0c9a3514f5746ffdc691589a0e6bc2df26c6522
balan-dot <balan@mail.uc.edu>
2026-09-17 14:03:53 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,114 @@
+public class Main {
+    static void main()
+        {
+            // int number with no fraction can be negative
+            // double is a number with a fractional part
+            int age = 0;
+            double salary = 23.50;
+            String name = " Tom ";
+            String greeting = " Hello World ";
+            boolean isRaining = false;
+            IO.println(" Hello World ");
+
+            IO.println(greeting);
+
+            IO.println(isRaining);
+
+
+            IO.println(" Hello World "); // Literal
+            // IO.print("Hello World");
+
+
+            int intOperandA = 23;
+            int intOperandB = 17;
+            int intSum = 0;
+            int intProduct = 0;
+            int intDifference = 0;
+            int intQuotient = 0;
+            int intModulo = 0;
+
+            intSum = intOperandA + intOperandB; // sum of 23 and 17
+            IO.println("The sum of " + intOperandA + " + " + intOperandB + " = " + intSum);
+
+            intProduct = intOperandA * intOperandB;
+            IO.println("The product of " + intOperandA + " * " + intOperandB + " = " + intProduct);
+
+
+            intDifference = intOperandA - intOperandB;
+            IO.println("The difference of " + intOperandA + " - " + intOperandB + " = " + intDifference);
+
+            intQuotient = intOperandA / intOperandB;
+            IO.println("The quotient of " + intOperandA + " / " + intOperandB + " = " + intQuotient);
+
+            intModulo = intOperandA % intOperandB;
+            IO.println("The remainder of " + intOperandA + " % " + intOperandB + " = " + intModulo);
+
+
+
+
+
+
+
+            double doubleOperandA = 16.50;
+            double doubleOperandB = 24.50;
+            double doubleSum = 0;
+            double doubleProduct = 0;
+            double doubleDifference = 0;
+            double doubleQuotient = 0;
+            double doubleModulo = 0;
+
+            doubleSum = doubleOperandA + doubleOperandB;
+            IO.println("The sum of " + doubleOperandA + " + " + doubleOperandB + " = " + doubleSum);
+
+            doubleProduct = doubleOperandA * doubleOperandB;
+            IO.println("The product of " + doubleOperandA + " * " + doubleOperandB + " = " + doubleProduct);
+
+            doubleDifference = doubleOperandA - doubleOperandB;
+            IO.println("The difference of " + doubleOperandA + " - " + doubleOperandB + " = " + doubleDifference);
+
+            doubleQuotient = doubleOperandA / doubleOperandB;
+            IO.println("The quotient of " + doubleOperandA + " / " + doubleOperandB + " = " + doubleQuotient);
+
+            doubleModulo = doubleOperandA % doubleOperandB;
+            IO.println("The modulo of " + doubleOperandA + " % " + doubleOperandB + " = " + doubleModulo);
+
+            int FamilyKidCount = 3;
+            System.out.println("The number of kids in my family is: " + FamilyKidCount);
+
+            boolean rainOutside = false;
+            System.out.println("Martha is it raining outside right now?: " + rainOutside);
+
+            double gallonGasPrice = 3.29;
+            System.out.println("The cost I paid for a gallon of gas today was: " + gallonGasPrice);
+
+            int preferredNumber = 8;
+            System.out.println("My favorite number ever is: " + preferredNumber);
+
+            double currentShoeSize = 9.5;
+            System.out.println("My current shoe size is: " + currentShoeSize);
+
+            String monthBorn = "December";
+            System.out.println("The month I was born in is: " + monthBorn);
+
+            String studentFullName = "Aaryan Bal";
+            System.out.println("My full name is: " + studentFullName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
